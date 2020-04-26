65823ac2a5cb514b5b732bc730f7977e2c27b623
Fernando <portillofernando92@gmail.com>
2020-04-25 21:10:07 -0500

    - pre-loader - background changed based on day/night cycle

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
-//Write Scripts Here
+//pre-loader
+$(function () {
+    $(window).on("load", function () {
+        $('.preloader').fadeOut('slow', function () {
+            $(this).remove();
+        });
+        $("body").removeClass("scroll-hidden");
+    });
+});
+
+$(document).ready(() => {
+
+    //set background and change text color
+    function setBackground() {
+        var today = new Date()
+        var date = new Date().toDateString()
+        var h = today.getHours()
+        if (6 <= h && h < 17) {
+            //Morning
+            $(".container-fluid").css("background-image", "url(" + "../images/backgrounds/discord-background-day.jpg" + ")")
+        }else if (h >= 17) {
+            //night
+            $(".container-fluid").css("background-image", "url(" + "../images/backgrounds/discord-background-night.png" + ")")
+            $(".container-fluid").css("color", "white")
+            console.log("Night")
+        }
+    }
+
+    setBackground()
+})
