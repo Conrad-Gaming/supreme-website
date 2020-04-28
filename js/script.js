75653313de623e4af4cbbb8d450134c2de1b8e55
Fernando <portillofernando92@gmail.com>
2020-04-28 16:22:29 -0500

    - add more pages

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 //pre-loader
 $(function () {
+    $("body").addClass("scroll-hidden");
+    $("container-fluid").addClass("scroll-hidden");
+    
     $(window).on("load", function () {
         $('.preloader').fadeOut('slow', function () {
             $(this).remove();
         });
         $("body").removeClass("scroll-hidden");
+        $(".container-fluid").removeClass("scroll-hidden");
     });
 });
 
@@ -17,10 +21,10 @@ $(document).ready(() => {
         var h = today.getHours()
         if (6 <= h && h < 17) {
             //Morning
-            $(".container-fluid").css("background-image", "url(" + "../images/backgrounds/discord-background-day.jpg" + ")")
+            $(".container-fluid").css("background-image", "url(" + "images/backgrounds/discord-background-day.jpg" + ")")
         }else if (h >= 17) {
             //night
-            $(".container-fluid").css("background-image", "url(" + "../images/backgrounds/discord-background-night.png" + ")")
+            $(".container-fluid").css("background-image", "url(" + "images/backgrounds/discord-background-night.png" + ")")
             $(".container-fluid").css("color", "white")
             console.log("Night")
         }
