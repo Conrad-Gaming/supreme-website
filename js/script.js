0de6fad853d51bc97732289c837c16207d9cb5fb
Fernando <portillofernando92@gmail.com>
2020-05-22 12:42:56 -0500

    - many font change

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 $(function () {
     $("body").addClass("scroll-hidden");
     $("container-fluid").addClass("scroll-hidden");
-    
+
     $(window).on("load", function () {
         $('.preloader').fadeOut('slow', function () {
             $(this).remove();
@@ -22,7 +22,7 @@ $(document).ready(() => {
         if (6 <= h && h < 17) {
             //Morning
             $(".container-fluid").css("background-image", "url(" + "images/backgrounds/discord-background-day.jpg" + ")")
-        }else if (h >= 17) {
+        } else if (h >= 17) {
             //night
             $(".container-fluid").css("background-image", "url(" + "images/backgrounds/discord-background-night.png" + ")")
             $(".container-fluid").css("color", "white")
@@ -31,4 +31,21 @@ $(document).ready(() => {
     }
 
     setBackground()
+})
+
+//form test
+var $form = $('form#test-form'),
+    url = 'https: //script.google.com/macros/s/AKfycbxtlV9G_sLx2a0A7EXltQYW7lzSFMLXmKyMdjyKyRu5jtOTaHZ2/exec'
+
+$('#submit-form').on('click', function (e) {
+    e.preventDefault();
+    var jqxhr = $.ajax({
+        url: url,
+        method: "GET",
+        dataType: "json",
+        data: $form.serializeObject()
+    }).success(
+        // do something
+
+    );
 })
